5f6d2cd237624847a5aa1e79475ded7b41d365bc
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-04-28 17:25:54 +0530

    new changes

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -1,11 +1,25 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+async function getData(url: string) {
+  const baseUrl = "http://localhost:1337";
+  try {
+    const response = await fetch(baseUrl + url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getData("/api/home-page");
+  console.log(strapiData);
+  const {Title, Description} = await strapiData.data.attributes;
   return (
     <main>
-      <Button>
-        New Shadcn Button
-      </Button>
+      <h1>{Title}</h1>
+      <p>{Description}</p>
     </main>
   );
 }
